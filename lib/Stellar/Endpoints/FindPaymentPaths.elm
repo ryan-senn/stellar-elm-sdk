7f8ca95e2833e742a4c60cbfe2b1d6398a99688e
ryan senn <ryan.senn.au@gmail.com>
2018-07-31 09:08:47 +1000

    add mandatory & optional fields to fiend payment request

--- lib/Stellar/Endpoints/FindPaymentPaths.elm
+++ lib/Stellar/Endpoints/FindPaymentPaths.elm
@@ -5,18 +5,24 @@ import HttpBuilder exposing (..)
 
 import Json.Decode as Decode exposing (Decoder)
 
-import Stellar.Endpoint as Endpoint exposing (Endpoint)
+import Stellar.Endpoint exposing (Endpoint)
+import Stellar.PublicKey exposing (PublicKey)
+import Stellar.AssetType exposing (AssetType)
 
 import Stellar.Resources.Operation as Operation exposing (Operation)
 
 import Stellar.Error as Error exposing (Error)
 
 
-requestBuilder : Endpoint -> Int -> RequestBuilder Response
-requestBuilder endpoint operationId =
+requestBuilder : Endpoint -> PublicKey -> PublicKey -> AssetType -> Int -> RequestBuilder Response
+requestBuilder endpoint sourceAccount destinationAccount assetType amount =
 
-    HttpBuilder.get (url endpoint operationId)
+    HttpBuilder.get (url endpoint)
         |> withExpect (Http.expectJson decoder)
+        |> withQueryParams [("source_account", sourceAccount)]
+        |> withQueryParams [("destination_account", destinationAccount)]
+        |> withQueryParams [("destination_asset_type", assetType)]
+        |> withQueryParams [("destination_amount", amount)]
 
 
 send : (Result Http.Error Response -> msg) -> RequestBuilder Response -> Cmd msg
@@ -24,10 +30,22 @@ send =
     HttpBuilder.send
 
 
-url : Endpoint -> Int -> String
-url endpoint operationId =
+setDestinationAssetCode : String -> RequestBuilder Response -> RequestBuilder Response
+setDestinationAssetCode destinationAssetCode requestBuilder =
+    requestBuilder
+        |> withQueryParams [("destination_asset_code", destinationAssetCode)]
 
-    endpoint ++ "/operations/" ++ toString operationId
+
+setDestinationAssetIssuer : PublicKey -> RequestBuilder Response -> RequestBuilder Response
+setDestinationAssetIssuer destinationAssetIssuer requestBuilder =
+    requestBuilder
+        |> withQueryParams [("destination_asset_issuer", destinationAssetIssuer)]
+
+
+url : Endpoint -> String
+url endpoint =
+
+    endpoint ++ "/paths"
 
 
 type Response
